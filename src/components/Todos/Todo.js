6a2c428adde6f7a16518fa2d57a04e5b02be0889
Eugene-prog <ieugene.prog@gmail.com>
2023-05-08 22:22:38 +0700

    refactor: added unique uuid key for todo, changed structure of todo in array object {text: text, isComplited: false,  id: uuid}

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,12 +2,12 @@ import  { RiTodoFill } from 'react-icons/ri';
 import styles from "./Todo.module.css";
 
 
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo }) {
   return  (
-    <div className={styles.todo}>
+    <div className={styles.todo} onDoubleClick={()=>deleteTodo(todo.id)}>
       <RiTodoFill className={styles.todoIcon}/>
-      <div className={styles.todoText} onDoubleClick={()=>deleteTodo(index)}>
-        {todo}
+      <div className={styles.todoText}>
+        {todo.text}
       </div>
     </div>
   )
