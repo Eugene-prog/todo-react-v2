b0dfeb815cc41952ab42b5c4c1eda1293d58955d
Eugene-prog <ieugene.prog@gmail.com>
2023-05-09 20:22:49 +0700

    feat: adding a block with buttons reset and clear completed todos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
+import TodosActions from './components/Todos/TodosActions'
+
 import './App.css';
 
 function App() {
@@ -30,6 +32,7 @@ function App() {
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler}/>
+      <TodosActions/>
       <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler}/>
     </div>
   );
