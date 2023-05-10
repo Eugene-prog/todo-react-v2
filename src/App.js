9fede0abb03f58faf56b5df9643f38ea6b7eea5b
Eugene-prog <ieugene.prog@gmail.com>
2023-05-10 21:25:15 +0700

    feat: disable button to delete completed tasks on condition of no completed tasks

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
-
+ 
   const addTodoHandler = (text) => {
     const newTodo = {
       text: text,
@@ -17,6 +17,8 @@ function App() {
     }
     setTodos([...todos, newTodo]);
   }
+
+  const completedTodoCount = todos.filter((todo)=>todo.isComplited).length;
 
   const deleteTodoHandler = (id) => {
     setTodos(todos.filter((todo) => todo.id!==id));
@@ -40,8 +42,9 @@ function App() {
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler}/>
-      {todos.length > 0 && <TodosActions resetTodo={resetTodoHandler} deleteCompletedTodos = {deleteCompletedTodosHandler}/>}
+      {todos.length > 0 && <TodosActions complitedTodosExist={!!completedTodoCount} resetTodo={resetTodoHandler} deleteCompletedTodos = {deleteCompletedTodosHandler}/>}
       <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler}/>
+      {completedTodoCount > 0 && <h2>You have {completedTodoCount} completed Todo</h2>}
     </div>
   );
 }
--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,12 +1,13 @@
 import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri'; 
 import Button from '../UI/Button';
+import styles from './TodosAction.module.css';
 
-function TodosActions({resetTodo, deleteCompletedTodos}) {
+function TodosActions({ resetTodo, deleteCompletedTodos, complitedTodosExist }) {
   return (
-    <>
+    <div className={styles.todosActionsContainer}>
     <Button title={"Reset Todos"} onClick={resetTodo}><RiRefreshLine/></Button>
-    <Button title={"Clear Completed Todos"} onClick={deleteCompletedTodos}><RiDeleteBin2Line/></Button>
-    </>
+    <Button title={"Clear Completed Todos"} onClick={deleteCompletedTodos} disabled={!complitedTodosExist}><RiDeleteBin2Line/></Button>
+    </div>
   )
 }
 
